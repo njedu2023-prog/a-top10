e0618b3f4090f41a963de9974a39b0bf8f6357ee
njedu2023-prog <njedu2023@gmail.com>
2026-02-04 20:44:53 +0800

    Create __main__.py

---
+++ a_top10/__main__.py
@@ -0,0 +1,12 @@
+# a_top10/__main__.py
+from __future__ import annotations
+
+from a_top10.main import run_pipeline
+
+
+def main() -> None:
+    run_pipeline()
+
+
+if __name__ == "__main__":
+    main()
